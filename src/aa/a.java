93dda0fd69604d4fee3f36015d329b8cf957af3c
kawamoriakihisa <kawamoriakihisa@localhost>
2025-05-12 15:02:22 +0900

    first

---
+++ src/aa/a.java
@@ -0,0 +1,28 @@
+package aa;
+import java.util.ArrayList;
+
+
+public class a {
+
+	public static void main(String[] args) {
+		ArrayList<String> fruits = new ArrayList<>();
+		
+		fruits.add("apple");
+		System.out.println(fruits);
+		int age = 20;
+		String name = "たろう";
+		int yoko = 4;
+		int tate = 6;
+		System.out.println("横幅" + yoko + "横幅" + tate + "の長方形の面積は" + yoko * tate);
+//		System.out.println(age + name);
+		boolean a = false;
+		char b = 'A';
+		float c = 6.28f;
+		long d = 123456789012345L;
+		String e = "プログラミングは楽しい!";
+//		System.out.println("a" + "b" + "c" + "d" + "e");
+//		double d = 5.8;
+//		int i = (int)d;
+//		System.out.println(d);
+	}
+}
