9236465f4b1ed8615a313c24c8ccfc865887a2c2
kawamoriakihisa <kawamoriakihisa@localhost>
2025-05-14 12:00:08 +0900

    5

--- src/aa/chapter5.java
+++ src/aa/chapter5.java
@@ -1,8 +1,23 @@
 package aa;
 
+import java.util.ArrayList;
+
 public class chapter5 {
 	public static void greet(String message) {
 		System.out.println(message);
-		greet("こんにちは");
+		
+		ArrayList<String> fruits = new ArrayList<>();
+		fruits.add("apple");
+		fruits.add(1, "banana");
+		fruits.add(2, "greap");
+		
+		String firstFruits = fruits.get(1);
+		System.out.println(firstFruits);
+		
+		fruits.set(0, "melon");
+		
+		int count = fruits.size();
+		
+		System.out.println(count);
 	}
 }
