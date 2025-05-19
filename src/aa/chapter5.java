2840977e32db811ed3d6088ec55c613266032666
kawamoriakihisa <kawamoriakihisa@localhost>
2025-05-19 14:58:18 +0900

    コミット

--- src/aa/chapter5.java
+++ src/aa/chapter5.java
@@ -3,9 +3,7 @@ package aa;
 import java.util.ArrayList;
 
 public class chapter5 {
-	public static void greet(String message) {
-		System.out.println(message);
-		
+	public static void main(String[] args) {
 		ArrayList<String> fruits = new ArrayList<>();
 		fruits.add("apple");
 		fruits.add(1, "banana");
@@ -18,6 +16,33 @@ public class chapter5 {
 		
 		int count = fruits.size();
 		
+		if(count > 0) {
+			fruits.clear();
+		}
 		System.out.println(count);
+		
+		// インスタンス化
+		// 演習1
+		Player player = new Player();
+		
+		String language = "java";
+		
+		Integer  value = 42;
+		
+		ArrayList<String> items = new ArrayList<>();
+		
+		System.out.println("プレイヤーを作成しました");
+		System.out.println("使用言語:" + language);
+		System.out.println("値:" + value);
+		System.out.println("アイテム数:" + items.size());
+	}
+	
+	class Player() {
+		
+	}
+	public static void greet(String message) {
+		System.out.println(message);
+		
+		
 	}
 }
