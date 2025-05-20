abd970d0351fa969ac7a50116abf8fbfd1427f09
kawamoriakihisa <kawamoriakihisa@localhost>
2025-05-20 12:39:19 +0900

    commit

--- src/aa/Main.java
+++ src/aa/Main.java
@@ -2,6 +2,19 @@ package aa;
 
 public class Main {
 	public static void main(String... args) {
-		System.out.println("あいうえお");
+		System.out.println("11まで進めてたので12からやってます");
+		Integer i = 5;
+		if (i == 5) {
+			System.out.println("iは5");
+		}
+		
+		
+		if (i == 3) {
+			System.out.println("iは3");
+		}else {
+			System.out.println("iは3ではない");
+		}
+		
+		
 	}
 }
