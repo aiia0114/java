daf270898d5867a387ff47bc2989de2c0a07146e
kawamoriakihisa <kawamoriakihisa@localhost>
2025-05-20 13:03:52 +0900

    commit

--- src/aa/Main.java
+++ src/aa/Main.java
@@ -1,4 +1,5 @@
 package aa;
+import java.util.ArrayList;
 
 public class Main {
 	public static void main(String... args) {
@@ -15,6 +16,25 @@ public class Main {
 			System.out.println("iは3ではない");
 		}
 		
+		if (i == 1) {
+            System.out.println("1位です");
+        } else if (i == 2){
+            System.out.println("2位です");
+        } else {
+        	System.out.println("最下位です");
+        }
 		
+		ArrayList<Integer> socres = new ArrayList<>();
+		socres.add(30);
+		socres.add(70);
+		socres.add(90);
+		socres.add(69);
+		socres.forEach(score -> {
+			if (score >= 70) {
+				System.out.println("合格");
+			}else {
+				System.out.println("不合格");
+				}
+		});
 	}
 }
